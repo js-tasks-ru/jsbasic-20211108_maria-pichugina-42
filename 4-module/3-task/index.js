f356429d6ed72e39c6c486f05c2379ce05a30a33
Maria <maria.pichugina.42@gmail.com>
2021-11-26 21:49:57 +0300

    тяжело

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,28 @@
 function highlight(table) {
-  // ваш код...
+  const trows = table.lastElementChild.getElementsByTagName("tr");
+  for (const element of trows) {
+    if (element.lastElementChild.hasAttribute("data-available")) {
+      if (element.lastElementChild.getAttribute("data-available") === "true") {
+        element.classList.add("available");
+      } else {
+        element.classList.add("unavailable");
+      }
+    } else {
+      element.setAttribute("hidden", "");
+    }
+    const tds = element.getElementsByTagName("td");
+    for (const td of tds) {
+      const n = Number(td.innerHTML);
+      if (isFinite(n) && n < 18) {
+        element.style.textDecoration = "line-through";
+      }
+      if (td.innerHTML === "f") {
+        element.classList.add("female");
+      }
+
+      if (td.innerHTML === "m") {
+        element.classList.add("male");
+      }
+    }
+  }
 }
